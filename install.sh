ce5e68d6efc5908d06fe1259e7048fe8dae81d94
Yuje Lee <yuje.lee@nota.ai>
2023-02-21 10:24:23 +0000

    update install packages

---
+++ install.sh
@@ -0,0 +1,7 @@
+pip install torch==1.7.0+cu110 torchvision==0.8.0+cu110 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install tensorflow==2.8.0
+pip install onnx==1.12.0
+pip install onnx-tf==1.9.0
+pip install numpy==1.21.0
+pip install keras==2.8.0
+pip install protobuf==3.19.6
